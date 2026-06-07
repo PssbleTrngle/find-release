894569a8cf9d2c045e10070327b5dfa2f90567ca
Niklas Widmann <nik@somethingcatchy.net>
2026-06-07 21:47:27 +0200

    fetch own repositories releases

--- test/finder.test.ts
+++ test/finder.test.ts
@@ -2,14 +2,12 @@ import { getOctokit } from "@actions/github";
 import { expect, it } from "bun:test";
 import findRelease from "../src/finder";
 
+const repo = { owner: "PssbleTrngle", repo: "find-release" };
+
 it("finds latest draft", async () => {
   const octokit = getOctokit(process.env.GITHUB_TOKEN!!);
 
-  const actual = await findRelease(
-    octokit,
-    { owner: "PssbleTrngle", repo: "TestMod" },
-    { type: "draft" },
-  );
+  const actual = await findRelease(octokit, repo, { type: "draft" });
 
   expect(actual).not.toBeNull();
 });
@@ -17,11 +15,9 @@ it("finds latest draft", async () => {
 it("finds latest for branch", async () => {
   const octokit = getOctokit(process.env.GITHUB_TOKEN!!);
 
-  const actual = await findRelease(
-    octokit,
-    { owner: "PssbleTrngle", repo: "TestMod" },
-    { branch: "main/common/1.21.x" },
-  );
+  const actual = await findRelease(octokit, repo, {
+    branch: "main/common/1.21.x",
+  });
 
   expect(actual).not.toBeNull();
 });
